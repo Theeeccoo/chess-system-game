58264918ed85d2b0be606d2137abeecd5870d3de
Theeeccoo <thiagodecampos1@gmail.com>
2022-02-16 17:17:58 -0300

    Creating method that shows which are the possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,10 +21,14 @@ public class Program {
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                UI.clearScreen();
+
 
             } catch (ChessException e){
                 System.out.println(e.getMessage());
