28c0a0c1fd1a3dbd803f0c29f0be0f26394abacb
Theeeccoo <thiagodecampos1@gmail.com>
2022-03-04 16:11:40 -0300

    ADDED Bishop Piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -159,8 +160,7 @@ public class ChessMatch {
     private boolean testCheckMate(Color color){
         if (!testCheck(color)){
             return false;
-        }
-        if(testCheck(color)){
+        } else {
             List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
             for ( Piece p : list ) {
                 boolean[][] mat = p.possibleMoves();
@@ -201,6 +201,8 @@ public class ChessMatch {
         placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
@@ -214,6 +216,8 @@ public class ChessMatch {
         placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 
 
     }
